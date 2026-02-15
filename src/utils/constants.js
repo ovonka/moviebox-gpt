826c3629a2ad34595c9808713d6c98f45650bff5
Nkanyiso Ntshangase <102148695+ovonka@users.noreply.github.com>
2026-02-15 15:24:33 +0200

    feat: add constants for logo and background images

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const LOGO_URL = "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg";
+export const LOGIN_BG_URL ="https://xboxwire.thesourcemediaassets.com/sites/2/2023/05/Background-size1920x1080-4e1694a6-75aa-4c36-9d4d-7fb6a3102005-bc5318781aad7f5c8520.png";
+export const PROFILE_ICON_URL = "https://avatars.githubusercontent.com/u/102148695?v=4";
+export const BROWSE_BG_URL = "https://images.unsplash.com/photo-1627873649417-c67f701f1949?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG5ldGZsaXh8ZW58MHx8MHx8fDA%3D";
